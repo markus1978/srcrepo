7d8ebd362209ab4a1870f0546d75cf7fa2c94f67
smoovie <phoenix_ada@web.de>
2014-12-27 14:36:31 +0100

    expanded switch

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest.java
@@ -2,7 +2,16 @@ package testclasses;
 
 public class McCabeTest {
 	
-	static void wochentagsName(int nummer) {
-		 
-		}	
+	static String wochentagsName(int nummer) {
+		switch(nummer) {
+		    case 1: return "Montag";
+		    case 2: return "Dienstag";
+		    case 3: return "Mittwoch";
+		    case 4: return "Donnerstag";
+		    case 5: return "Freitag";
+		    case 6: return "Samstag";
+		    case 7: return "Sonntag";
+		  }
+		return "unknown";
+	}	
 }
