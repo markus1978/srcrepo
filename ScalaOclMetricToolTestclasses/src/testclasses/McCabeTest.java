6407efdcf15b19184a868dd8e8321d65e1fa2979
smoovie <phoenix_ada@web.de>
2014-12-27 15:29:31 +0100

    added a seconds switch construct for testing

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest.java
@@ -14,4 +14,17 @@ public class McCabeTest {
 		  }
 		return "unknown";
 	}	
+	
+	static String wochentagsName2(int nummer) {
+		switch(nummer) {
+		    case 1: return "Montag2";
+		    case 2: return "Dienstag2";
+		    case 3: return "Mittwoch2";
+		    case 4: return "Donnerstag2";
+		    case 5: return "Freitag2";
+		    case 6: return "Samstag2";
+		    case 7: return "Sonntag2";
+		  }
+		return "unknown";
+	}	
 }
