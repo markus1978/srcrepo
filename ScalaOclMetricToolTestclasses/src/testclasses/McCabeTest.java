b5180262aab6bd5ddeebd73a676b690444cdbee1
smoovie <phoenix_ada@web.de>
2014-12-27 02:12:28 +0100

    init

---
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest.java
@@ -0,0 +1,10 @@
+package testclasses;
+@SuppressWarnings("javadoc")
+public class McCabeTest {
+	static String wochentagsName(int nummer) {
+		    
+		  switch(nummer) {	    
+		  }
+		  return "(unbekannter Wochentag)";
+		}	
+}
