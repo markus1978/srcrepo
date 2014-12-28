f260554ceeb335c2eeb3e8453e1269e98dc0f145
smoovie <phoenix_ada@web.de>
2014-12-28 16:12:29 +0100

    modified seconds cUnit and added commented blocks for testiing excludes

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest2.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTest2.java
@@ -12,5 +12,33 @@ public class McCabeTest2 {
 		  }
 		return "unknown";
 	}	
+	
+	static String wochentagsName2(int nummer) {
+		switch(nummer) {
+		    case 1: return "Montag2";
+		    case 2: return "Dienstag2";
+		    case 3: return "Mittwoch2";
+		    case 4: return "Donnerstag2";
+		    case 5: return "Freitag2";	
+		    case 6: return "Samstag2";
+		  }
+		return "unknown";
+	}
+	
+//	static String wochentagsName2(int nummer) {
+//		switch(nummer) {
+//		    case 1: return "Montag2";
+//		    case 2: return "Dienstag2";
+//		  }
+//		return "unknown";
+//	}
+	
+	/*static String wochentagsName2(int nummer) {
+		switch(nummer) {
+		    case 1: return "Montag2";
+		    case 2: return "Dienstag2";		
+		  }
+		return "unknown";
+	}*/
 
 }
