8a1445231bdedbffd10da1bb18ed4903b940e1ad
smoovie <phoenix_ada@web.de>
2014-12-28 17:16:26 +0100

    refactoring ResultObject to its own class

---
+++ ScalaOclMetricTool/src/de/hub/metrics/ResultObject.scala
@@ -0,0 +1,32 @@
+package de.hub.metrics
+
+import scala.collection.mutable.ListBuffer
+
+class ResultObject {
+  private var values:ListBuffer[Double] = new ListBuffer[Double];
+  private var fileName:String = "";
+  	  
+  override def toString():String = {	    
+    var string = "";	    
+    for (iter <- values.toList.iterator) {	    
+		string = string.concat(iter.toString);			
+	}
+    return string;
+  }
+  
+  def getFileName():String = {
+    return this.fileName ;
+  }
+  
+  def setFileName(name:String) = {
+    this.fileName = name;
+  }
+  
+   def getValues() : ListBuffer[Double] = {
+    return this.values;
+  }
+   
+   def setValues(list : ListBuffer[Double]) = {
+     this.values = list;
+	   }
+}
