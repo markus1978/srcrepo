a6f3bac29f381924fc775aa0eedaf199472b9d2c
marticke <marticke@Informatik.hu-berlin.de>
2014-12-29 13:49:29 +0100

    try to analyze strange behaviour on linux

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeTestSwitchCase.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTestSwitchCase.java
@@ -9,8 +9,6 @@ public class McCabeTestSwitchCase {
 		    case 3: return "Mittwoch";
 		    case 4: return "Donnerstag";
 		    case 5: return "Freitag";
-		    case 6: return "Samstag";
-		    case 7: return "Sonntag";
 		  }
 		return "unknown";
 	}	
