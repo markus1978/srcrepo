bd630d5e5418c6ec491effa8b15f3897e77bb07f
smoovie <phoenix_ada@web.de>
2015-01-03 15:29:06 +0100

    added methods for while, dowhile, for with logicalOperators

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeLogicalOperatorsTest.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeLogicalOperatorsTest.java
@@ -4,20 +4,53 @@ public class McCabeLogicalOperatorsTest {
 	
 	/**
 	 * Tester to find out how to caculated operators like &&, || correctly
-	 * Should give a McCabe value of 5 
+	 * Should give a McCabe value of 9 
 	 * = 1 binary branch + &&[what is the same like an if] 
 	 * + 1 binary branch + ||[what is the same like an if]
+	 * + 1 while[same like if] + ||[what is the same like an if]
+	 * + 1 while[same like if] + &&[what is the same like an if]
 	 * + 1
 	 * @param pA
 	 * @param pB
 	 */
-	static void whileTest(int pA, int pB){
+	static void logicalOperatorsTest(int pA, int pB){
 		int a = pA;
 		int b = pB;
 		if(a!=b && a>b)			
 			System.out.println("a!=b");		
 		if(a!=b || a>b)			
 			System.out.println("a!=b");
+		while(a!=b || a>b)			
+			System.out.println("a!=b");
+		while(a!=b && a>b)			
+			System.out.println("a!=b");
+	}
+	
+	/**
+	 * Tester to find out how to caculated operators like &&, || correctly
+	 * Should give a McCabe value of 9 
+	 * = + 1 for[same like if] + ||[what is the same like an if]
+	 * + 1 for[same like if] + &&[what is the same like an if]
+	 * + 1 do[same like if] + ||[what is the same like an if]
+	 * + 1 do[same like if] + &&[what is the same like an if]
+	 * + 1
+	 * @param pA
+	 * @param pB
+	 */
+	static void logicalOperatorsTest2(int pA, int pB){
+		int a = pA;
+		int b = pB;
+		for(a=0; a<5 && 0<b; a++)			
+			System.out.println("a!=b");		
+		for(a=0; a<5 || 0<b; a++)			
+			System.out.println("a!=b");
+		do
+			System.out.println("a!=b");
+		while(a!=b && a>b);
+		do
+			System.out.println("a!=b");
+		while(a!=b || a>b);
+			
 	}
 
 }
