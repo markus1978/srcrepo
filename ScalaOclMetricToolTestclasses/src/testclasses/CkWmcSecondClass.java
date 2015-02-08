aed85e1aaf2c7e7ae849cc0aec5889e96f14eea9
smoovie <phoenix_ada@web.de>
2015-02-08 14:12:21 +0100

    added a second class coupling

--- ScalaOclMetricToolTestclasses/src/testclasses/CkWmcSecondClass.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/CkWmcSecondClass.java
@@ -5,8 +5,11 @@ public class CkWmcSecondClass {
 	public static int WmcVariableAccess = 1;
 
 	public static void main(String[] args) {
+		//CkWmcSeconds becomes coupled to CkWmcTest by calling its contructor => +1 
 		CkWmcTest t = new CkWmcTest();
+		//the methodcall would be another coupling, but the class was already count, therefore the value stays unchanged
 		t.WmcTestPublic();
+		McCabeForTest.forTest(1, 3);
 	}
 
 }
