8e89b380ec4f09861968f0ae7b3620ac4e9ddba8
smoovie <phoenix_ada@web.de>
2015-03-27 14:48:34 +0100

    [McCabe_Metric] added Testclass for ternary "?:" operator

---
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeTernaryOperatorTest.java
@@ -0,0 +1,32 @@
+package testclasses;
+
+/**
+ * @author Frederik Marticke
+ *
+ */
+public class McCabeTernaryOperatorTest {
+
+	
+	/**
+	 * Should give a McCabe metric of 2 = 1 binary branch + 1
+	 * @return
+	 */
+	public int simpleTernaryTest() {
+		int i = 0;
+		int j = 1;
+		return i > j ? i : j;
+	}
+
+	/**
+	 * should give a McCabe metric of 4 = 3 binary branches + 1
+	 * @return
+	 */
+	public boolean complexTernaryTest() {
+		int i = 0;
+		if (i > 0)
+			return i > 10 ? (i > 100 && ((i + 10) < 200)) : true;
+		else
+			return false;
+	}
+
+}
