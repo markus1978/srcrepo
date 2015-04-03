571ff896ead1277eb32e5ff186143cbef4b3766e
smoovie <phoenix_ada@web.de>
2015-04-03 16:29:10 +0200

    [McCabe_Metric] Added Testclass for Exceptions.

---
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeExceptionsTest.java
@@ -0,0 +1,34 @@
+package testclasses;
+
+public class McCabeExceptionsTest {
+	
+	/**
+	 * Should give McCabe value = 3: 1 inner return + 1 catch + 1
+	 * @return
+	 */
+	public String tryCatchTest(){
+		String[] test = new String[2];
+		try{
+			String foo = test[-1];
+			return foo;
+		} catch(IndexOutOfBoundsException e){
+			System.out.println("Exception");
+		}
+		return "bar";
+	}
+	
+	/**
+	 * Should give McCabe value = 4 : 1 inner return + 1 catch + 1 throw + 1
+	 * @return
+	 * @throws Exception
+	 */
+	public String throwsTest() throws Exception{
+		String[] test = new String[2];
+		try{
+			String foo = test[-1];
+			return foo;
+		} catch(IndexOutOfBoundsException e){
+			throw new Exception("Error found");
+		}		
+	}
+}
