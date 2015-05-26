b4491b1e35c264f201fee784257557419606cefc
smoovie <phoenix_ada@web.de>
2015-05-26 17:57:50 +0200

    [CKJM] testclasses.jar und ps1 Skript

---
+++ ScalaOclMetricToolTestclasses/ckjm.ps1
@@ -0,0 +1,9 @@
+﻿$sw = New-Object System.Diagnostics.Stopwatch
+$Dir = 'C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\bin\testclasses'
+$classes = Get-ChildItem -Path $Dir
+$fc = ForEach( $File in $classes ) { $File.Fullname }
+
+$sw.Start()
+ForEach($item in $fc) {java -jar C:\Users\Worm\Programme\Ckjm_1.9\ckjm-1.9\build\ckjm-1.9.jar $item | Out-File C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ckjm.txt -Append}
+$sw.Stop()
+$sw.Elapsed.ToString() | Out-File C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ckjm.txt -Append
