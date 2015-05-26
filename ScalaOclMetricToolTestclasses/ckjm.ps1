e5a254727e592cb4adfbe97eccb5e662da2a8bcc
Frederik Marticke <phoenix_ada@web.de>
2015-05-26 18:59:34 +0200

    [CKJM] kleine skriptoptimierungen und ergebnis auf win7

--- ScalaOclMetricToolTestclasses/ckjm.ps1
+++ ScalaOclMetricToolTestclasses/ckjm.ps1
@@ -1,9 +1,15 @@
-﻿$sw = New-Object System.Diagnostics.Stopwatch
-$Dir = 'C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\bin\testclasses'
-$classes = Get-ChildItem -Path $Dir
-$fc = ForEach( $File in $classes ) { $File.Fullname }
+﻿$baseDir = 'C:\Users\Sera\git\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses'
+#$baseDir = 'C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses'
+
+$sw = New-Object System.Diagnostics.Stopwatch
+$classFileDir = "${baseDir}\bin\testclasses"
+$classes = Get-ChildItem -Path $classFileDir
+$fullPathToClassFiles = ForEach( $File in $classes ) { $File.Fullname }
 
 $sw.Start()
-ForEach($item in $fc) {java -jar C:\Users\Worm\Programme\Ckjm_1.9\ckjm-1.9\build\ckjm-1.9.jar $item | Out-File C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ckjm.txt -Append}
+ForEach($item in $fullPathToClassFiles) {
+    java -jar 'C:/Program Files/ckjm-1.9/ckjm-1.9/build/ckjm-1.9.jar' $item | Out-File ${baseDir}\ckjm2.txt -Append
+    #java -jar C:\Users\Worm\Programme\Ckjm_1.9\ckjm-1.9\build\ckjm-1.9.jar $item | Out-File ${baseDir}\ckjm.txt -Append
+}
 $sw.Stop()
-$sw.Elapsed.ToString() | Out-File C:\Users\Worm\Git_Workspace\Studienarbeit\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ScalaOclMetricToolTestclasses\ckjm.txt -Append
+$sw.Elapsed.ToString() | Out-File ${baseDir}\ckjm2.txt -Append
