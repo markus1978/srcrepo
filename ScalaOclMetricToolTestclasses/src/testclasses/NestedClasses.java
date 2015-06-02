7a81988423f87a2706fb4f6d2b9f2bb5e0657683
smoovie <phoenix_ada@web.de>
2015-06-02 20:41:35 +0200

    added a class with a nested class to see how the model looks like and how the metrics handle this.

---
+++ ScalaOclMetricToolTestclasses/src/testclasses/NestedClasses.java
@@ -0,0 +1,17 @@
+package testclasses;
+
+public class NestedClasses {
+
+	public class nested1{
+		String s1 = "string1";
+		
+		private void nestedMethod1(){
+			System.out.println("method1");
+		}
+	}
+	
+	public void outerMethod1(){
+		nested1 n1 = new nested1();
+		n1.nestedMethod1();
+	}
+}
