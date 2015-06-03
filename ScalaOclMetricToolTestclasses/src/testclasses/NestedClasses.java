a95939d54c76c828488ca1b06a3757d93aed43f0
smoovie <phoenix_ada@web.de>
2015-06-03 12:26:47 +0200

    added a nested2 class inside nested1 class to check if the metric goes deep enough.

--- ScalaOclMetricToolTestclasses/src/testclasses/NestedClasses.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/NestedClasses.java
@@ -2,15 +2,24 @@ package testclasses;
 
 public class NestedClasses {
 
-	public class nested1 extends CkNocTest implements CkDitInterfaceLevelOneWithNoParent{
+	public class nested1 extends CkNocTest implements
+			CkDitInterfaceLevelOneWithNoParent {
 		String s1 = "string1";
-		
-		private void nestedMethod1(){
+		nested2 n2 = new nested2();
+
+		private void nestedMethod1() {
 			System.out.println("method1");
+			n2.nestedMethod2();
+		}
+
+		private class nested2 {
+			private void nestedMethod2() {
+				// foo
+			}
 		}
 	}
-	
-	public void outerMethod1(){
+
+	public void outerMethod1() {
 		nested1 n1 = new nested1();
 		n1.nestedMethod1();
 	}
