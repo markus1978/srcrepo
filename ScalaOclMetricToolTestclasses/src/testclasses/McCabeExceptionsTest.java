1532d84a16f6b9eb0dc732af6ca7f70bf3e841ff
smoovie <phoenix_ada@web.de>
2015-06-19 14:13:32 +0200

    [McCabe] added method with two catch clauses.

--- ScalaOclMetricToolTestclasses/src/testclasses/McCabeExceptionsTest.java
+++ ScalaOclMetricToolTestclasses/src/testclasses/McCabeExceptionsTest.java
@@ -18,6 +18,26 @@ public class McCabeExceptionsTest {
 	}
 	
 	/**
+	 * Should give McCabe value = 3: 1 inner return + 1 catch + 1
+	 * @return
+	 */
+	public String tryCatchSideEffectTest(){
+		String[] test = new String[2];
+		try{
+			String foo = test[-1];
+			return foo;
+		} catch(IndexOutOfBoundsException e){
+			System.out.println("Exception");
+			return "failed";
+		} catch(Exception e){
+			System.out.println("Exception");
+			//maybe sideeffects...
+		}
+		//maybe do some stuff after catch
+		return "bar";
+	}
+	
+	/**
 	 * Should give McCabe value = 4 : 1 inner return + 1 catch + 1 throw + 1
 	 * @return
 	 * @throws Exception
